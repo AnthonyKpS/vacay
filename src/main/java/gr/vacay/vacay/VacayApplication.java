9fe304bf73c89a02012573aecd265c199127baa6
kps <anthonykaparounakis@gmail.com>
2021-05-28 01:15:00 +0300

    Initial commit

---
+++ src/main/java/gr/vacay/vacay/VacayApplication.java
@@ -0,0 +1,13 @@
+package gr.vacay.vacay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/vacay/vacay/VacayApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.vacay.vacay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
